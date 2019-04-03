0e9d2ad68c9d5c507eb76a85842db36b4aeb34f8
Ahmad Satriamulya <35530330+AhmadStrmlya@users.noreply.github.com>
2019-04-03 16:56:32 +0700

    integrating with CI

---
+++ application/views/footer_page.php
@@ -0,0 +1,28 @@
+	<footer>
+		<p>LOGO HERE</p>
+		<div class="sect">
+			<ul>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Media Gallery</a></li>
+			</ul>
+		</div>
+		
+		<div class="sect">
+			<ul>
+				<li><a href="#">Terms & Conditions</a></li>
+				<li><a href="#">Privacy Policy</a></li>
+			</ul>
+		</div>
+
+		<div class="sect">
+			<ul>
+				<li><a href="#">*isi gambar</a></li>
+				<li><a href="#">*isi gambar</a></li>
+				<li><a href="#">*isi gambar</a></li>
+				<li><a href="#">*isi gambar</a></li>
+			</ul>
+		</div>
+	</footer>
+</body>
+</html>
