c3f601c4f0b43ee9c710d3e518c283bf7252fe7d
Naufal Hilmiaji <nhilmiaji@gmail.com>
2019-04-07 02:00:58 +0700

    Homepage Update (07-04-2019)
    Update footer(home) + login page

--- Home (NEW)/js/landing.js
+++ Home (NEW)/js/landing.js
@@ -17,18 +17,10 @@ var carousel = document.querySelector('[data-carousel]');
 var slides = document.getElementsByClassName('carousel-cell');
 var flkty = new Flickity(carousel, options);
 
-flkty.on('scroll', function () {
-  flkty.slides.forEach(function (slide, i) {
-    var image = slides[i];
-    var x = (slide.target + flkty.x) * -1/3;
-    image.style.backgroundPosition = x + 'px';
-  });
-});
-
 //Parallax Effect on Mouse Move
  $(window).mousemove(function(e) {
     var change;
-    var xpos=e.clientX;var ypos=e.clientY;var left= change*20;
-    var  xpos=xpos*2;ypos=ypos*2;
-    $('.inner').css('right',(( 0+(xpos/300))+"px"));           
+    var xpos=e.clientX;
+    var ypos=e.clientY;
+    $('.inner').css('right',(( 0+(xpos/80))+"px"));           
   });
