1f18f2d932220816f221f4b60267348327e191be
Ahmad Satriamulya <35530330+AhmadStrmlya@users.noreply.github.com>
2019-04-13 23:53:46 +0700

    Create M_course.php

---
+++ application/models/M_course.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_web extends CI_Model {
+	
+	public function Getcourse(){
+		$this->db->select('*');
+		$this->db->from('course');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function hapus_course($sub_id)
+	{
+		$this->db->where('sub_id',$sub_id);
+		$this->db->delete('course');
+	}
+
+	public function edit_course($sub_id,$data)
+	{
+		$this->db->where('sub_id',$sub_id);
+		return $this->db->update('course',$data);
+	}
+
+	public function tambah_course($data)
+	{
+		return $this->db->insert('course',$data);	
+	}
+
+}
