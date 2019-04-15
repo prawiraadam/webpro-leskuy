b162e7545f0eb99d2cc94b8ca51906eb6185f9e3
Prawira Adam <35517608+prawiraadam@users.noreply.github.com>
2019-04-15 19:23:51 +0700

    View Admin
    View udah beres insyaallah

---
+++ application/views/AdminLogin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Login</title>
+	<link rel="stylesheet" type="text/css" href="../../assets/css/AdminLogin.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="box">
+		<h3 class="title">Leskuy!</h3>
+		<h4 class="title">Admin Page</h4>
+		<form>
+		  <div class="form-group">
+		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Admin ID">
+		  </div>
+		  <div class="form-group">
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		  <button type="submit" class="btn btn-primary">Login</button>
+		</form>
+	</div>
+</body>
+</html>
