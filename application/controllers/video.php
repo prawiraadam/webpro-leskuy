e9b6bb9fbd2df013b34606435740cfd9133b8e39
catlaughing <shadieqcool@gmail.com>
2019-04-16 00:15:23 +0700

    video lagi

--- application/controllers/video.php
+++ application/controllers/video.php
@@ -14,6 +14,7 @@ class video extends CI_Controller {
 	public function videos($course_id)
 	{
 		$data_video = $this->M_video->Get_Video($course_id);
-		$this->load->view('page_header');
-		$this->load->view('video_page',['data'=>$data_video]);
+		$this->load->view('header_view');
+        $this->load->view('video/video_page',['data'=>$data_video]);
+        $this->load->view('footer_view');
 	}
