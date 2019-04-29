18ff0b3a37c657b1499ac65037cf2ec9702ae165
catlaughing <shadieqcool@gmail.com>
2019-04-29 18:48:37 +0700

    buat heroku

--- application/controllers/subject.php
+++ application/controllers/subject.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class subject extends CI_Controller {
+class Subject extends CI_Controller {
 
 	public function __construct()
  	{
